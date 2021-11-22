efe38dcfe513ced1aedb69451bb5d6bbf0fc23c9
Toshihito Kikuchi <k.toshihito@yahoo.de>
2021-11-22 10:31:42 -0800

    Add crash ping symbolicator

--- assets/mozilla/third-party-modules/aggregator.js
+++ assets/mozilla/third-party-modules/aggregator.js
@@ -42,6 +42,36 @@ const Aggregator = class {
     this.data.push(data);
   }
 
+  pushCrashPing(item) {
+    const date = new Date(item.crash_date);
+    let data = {
+      index: this.data.length,
+      build_id: item.build_id,
+      crash_date: date.toISOString().slice(0, 10),
+      id: item.id,
+      osver: item.os_version,
+      ffver: item.version,
+      process: item.process_type,
+      reason: item.type,
+    };
+
+    if (typeof item.stack === 'string') {
+      const json = JSON.parse(item.stack);
+      if (json !== null) {
+        data.parsed_stack = json.frames;
+        data.stack_disp = `[${json.frames.length} frames]`;
+      }
+    }
+    if (typeof item.memory_map === 'string') {
+      const json = JSON.parse(item.memory_map);
+      if (json !== null) {
+        data.parsed_modules = json;
+      }
+    }
+
+    this.data.push(data);
+  }
+
   mainTable() {
     return this.data;
   }
@@ -85,32 +115,43 @@ const Aggregator = class {
   }
 
   getSymbolicateRequest(index) {
-    const br = '<br />';
-
     const frames = this.data[index].parsed_stack;
     const modules = this.data[index].parsed_modules;
     if (!(frames && modules)) {
-      return `No callstack${br}`;
+      return null;
     }
 
-    let mm = [];
-    modules.forEach(module => mm.push([
+    if (modules[0].debug_file) {
+      // Crash ping
+      const mm = modules.map(module => [module.debug_file, module.debug_id]);
+      const stacks = frames.map(frame => {
+          const module = modules[frame.module_index];
+          return module
+              ? [frame.module_index, Number(frame.ip) - Number(module.base_addr)]
+              : [-1, 0];
+        });
+      return {memoryMap: mm, stacks: [stacks]};
+    }
+
+    const mm = modules.map(module => [
       module.f0_,
-      // API does not accept an emnpty breakpad id.
-      module.f1_.length == 0 ? 'x' : module.f1_]));
-    let stacks = [];
-    frames.forEach(frame => stacks.push([
+      // API does not accept an empty breakpad id.
+      module.f1_.length == 0 ? 'x' : module.f1_]);
+    const stacks = frames.map(frame => [
       frame.f0_,
       // Offset can be 0n18446744073709552000, but it's too big for JS to handle.
       Number(frame.f1_).toString(16) == '10000000000000000'
-        ? 0xffffffff : frame.f1_]));
-
+        ? 0xffffffff : frame.f1_]);
     return {memoryMap: mm, stacks: [stacks]};
   }
 
   showSymbolicatedCallstack(control, index) {
     control.innerHTML = 'loading...';
     const req = this.getSymbolicateRequest(index);
+    if (!req) {
+      control.innerHTML = "No callstack";
+      return;
+    }
 
     fetch('https://symbolication.services.mozilla.com/symbolicate/v5',
           {method: 'POST',
